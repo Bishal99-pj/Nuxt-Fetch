18388bf2ddeb4b28972e027660d462dc414fc767
Bishal99-pj <bishal1999mishra@gmail.com>
2024-01-19 00:09:36 +0530

    setup: Tailwind CSS
    https://nuxt.com/modules/tailwindcss
    npx tailwindcss init -> config paths to enable intellisense

---
+++ components/molecules/Hello.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="text-3xl font-bold text-blue-500 bg-black text-center pt-10">
+        Hello Guys!
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>

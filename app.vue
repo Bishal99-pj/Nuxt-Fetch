3a3bfc619aa6f0a69e0bbe28c2e1be84f2d47ba0
Bishal99-pj <bishal1999mishra@gmail.com>
2024-01-21 02:05:48 +0530

    added: product cards - routing and fetching

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="px-6">
     <NuxtPage/>
   </div>
 </template>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>
-
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
+        <ProductCard v-for="p in products" :key="p.id" :product="p"/>
     </div>
 </template>
 
 <script setup>
+const BaseURL = 'https://fakestoreapi.com/products'
+
+const {data: products} = await useFetch(BaseURL)
 
 </script>
 

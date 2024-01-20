62ef4a56e8db60296f21725570b2d287700a13c5
Bishal99-pj <bishal1999mishra@gmail.com>
2024-01-21 02:05:48 +0530

    added: product cards - routing and fetching

--- app.vue
+++ app.vue
@@ -1,9 +1,5 @@
 <template>
-<<<<<<< feature/nuxt-fetch
   <div class="px-6">
-=======
-  <div>
->>>>>>> added: Iconify module
     <NuxtPage/>
   </div>
 </template>
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+    modules: [
+      '@nuxtjs/tailwindcss',
+      'nuxt-icon'
+    ],
+    components: [
+      {
+        path: '~/components',
+        pathPrefix: false,
+      },
+    ],
+    css : ['~/assets/css/global.css']  
+})
